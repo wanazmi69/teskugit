d4cb7686eae43c82b7224e04acb3e45e622bec75
wanazmi69 <lawtheoscar@gmail.com>
2022-07-03 14:11:34 +0900

    tamabahan script

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
 import 'tailwindcss/tailwind.css'
 
 
+
 export default function Home() {
+
+  const title = "tes upload srcipt"
   return (
     <div className="flex flex-col  justify-center items-center w-full h-screen bg-sky-300">
       <span className="text-4xl font-semibold text-sky-800">Tes Tailwin NEXT</span>
       <span className="text-grey-800">Di deploy melalu github dan firebase</span>
+
+      <span>{title}</span>
     </div>
   )
 }
